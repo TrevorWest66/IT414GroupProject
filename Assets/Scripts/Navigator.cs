e6509af4f601b610263c744fc5fbfed35573ba6a
Ellie McDonald <elliejeanmcd@gmail.com>
2021-03-13 20:28:20 -0600

    User Story 57

--- Assets/Scripts/Navigator.cs
+++ Assets/Scripts/Navigator.cs
@@ -1,6 +1,7 @@
-﻿//Written by Lance Graham
+﻿//Written by Lance Graham & Ellie McDonald
 //This is another model/controller responsible for coordinating the character's movement and location
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class Navigator : MonoBehaviour
 {
@@ -9,7 +10,48 @@ public class Navigator : MonoBehaviour
     private float speed = 3.0f;
     public  bool enableDisplay = false;
 
+    private XmlParser anXmlParser;
+    private KeyCode left, right, forward, backward, jump;
+
     private AbstractCalculate distanceCalculator = new ThreeDimensionalCalculate();
+
+    void Start()
+    {
+        // Dictates which controls will be used
+        BuilderDirector builderDirector = new BuilderDirector();
+        PlayerControlsBuilder controls;
+        if (true)
+        {
+            controls = new ArrowControlsBuilder();
+        }
+        if (false /*change to pull in settings from main menu settings*/)
+        {
+            controls = new WASDContolsBuilder();
+        }
+
+        builderDirector.Construct(controls);
+
+        //Parse the xml file to establish the controls that wil be used to move the capsule
+        //through the maze. The Contrlols.xml file is located in the Maze SandBox directory.
+        if (controls.GetType().ToString().Contains("ArrowControlsBuilder"))
+        {
+            anXmlParser = new XmlParser();
+            anXmlParser.loadDocument(controls.SetPlayerControls());
+            Debug.Log("Set player Controls: arrow");
+        }
+        else if (controls.GetType().ToString().Contains("WASDContolsBuilder"))
+        {
+            anXmlParser = new XmlParser();
+            anXmlParser.loadDocument(controls.SetPlayerControls());
+            Debug.Log("Set player Controls: wasd");
+        }
+
+        forward = anXmlParser.parseXml("forward");      // W or up arrow
+        backward = anXmlParser.parseXml("backward");    //S or down arow
+        left = anXmlParser.parseXml("left");            //A or left arrow
+        right = anXmlParser.parseXml("right");          //D or right arrow
+        jump = anXmlParser.parseXml("jump");            // Space bar
+    }
 
     //Update is called once per frame
     void Update()
@@ -21,29 +63,40 @@ public class Navigator : MonoBehaviour
         enableDisplay = distanceCalculator.Calculate(theCauldron, currentLocation);
 
         //Move Forward
-        if (Input.GetKeyUp(KeyCode.UpArrow))
+        if (Input.GetKeyUp(forward))
         {
             vMove = 10.0f;
         }
 
         //Move Back
-        if (Input.GetKeyUp(KeyCode.DownArrow))
+        if (Input.GetKeyUp(backward))
         {
             vMove = -10.0f;
         }
 
         //Turn Left
-        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        if (Input.GetKeyUp(left))
         {
-            transform.Rotate(0.0f, -90.0f, 0.0f);
+            transform.Rotate(0.0f, -10.0f, 0.0f);
         }
 
         //Turn Right
-        if (Input.GetKeyUp(KeyCode.RightArrow))
+        if (Input.GetKeyUp(right))
         {
             //transform is the game object
-            transform.Rotate(0.0f, 90.0f, 0.0f);
+            transform.Rotate(0.0f, 10.0f, 0.0f);
         }
+
+        // TODO: Do we want to implement this?
+        // Jump
+        /*if (Input.GetKeyUp(jump))
+        {
+            float yMove = 13.0f;
+
+            transform.position = new Vector3(transform.position.x, transform.position.y + yMove, transform.position.z);
+            async Task.Delay(System.TimeSpan.FromSeconds(wait));
+            transform.position = new Vector3(transform.position.x, transform.position.y + (0 - yMove), transform.position.z);
+        }*/
 
         Vector3 aMove = transform.forward * vMove + transform.right * hMove;
         this.GetComponent<CharacterController>().Move(aMove * Time.deltaTime * speed);
---
+++ Assets/Scripts/PlayerControlsBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class PlayerControlsBuilder
+{
+    public abstract string SetPlayerControls();
+}
+
+// Inherits from builder to have unique method executions
+public class ArrowControlsBuilder : PlayerControlsBuilder
+{
+    public override string SetPlayerControls()
+    {
+        return "ArrowControls.xml";
+    }
+
+}
+
+// Inherits from builder to have unique method executions
+public class WASDContolsBuilder : PlayerControlsBuilder
+{
+    public override string SetPlayerControls()
+    {
+        return "WASDControls.xml";
+    }
+
+}
+
+// Builder Director constructs the builder to execute the unique builder class public class BuilderDirector
+public class BuilderDirector
+{
+    public void Construct(PlayerControlsBuilder aBuilder)
+    {
+        aBuilder.SetPlayerControls();
+    }
+}
