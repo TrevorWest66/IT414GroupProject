7259175d386bfb5407ef2c32ba2e1b288bb291eb
Rebecca Henry <henry.rebeccarose@gmail.com>
2021-03-14 22:21:07 -0500

    Adds Plant Factory
    Not fully functioning

---
+++ Assets/Scripts/PlantFactory.cs
@@ -0,0 +1,18 @@
+﻿// Written by Rebecca Herny
+// Creates the plants
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantFactory : AbstractGameObjectFactory
+{
+    public override void CreateGameObject(Vector3 thePosition, int scale)
+    {
+        // Replace this with the plant obj
+        GameObject thePlant = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        thePlant.transform.localScale = new Vector3(scale, scale, scale);
+        thePlant.transform.position = thePosition;
+
+    }
+}
---
+++ Assets/Scripts/PopulatePlants.cs
@@ -0,0 +1,56 @@
+﻿// Written by Rebecca Henry
+
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public sealed class PopulatePlants : MonoBehaviour
+{
+    public LayerMask mask = -1;
+    float radius;
+
+    // Lists all the plants populated on the map
+    List<GameObject> PlantsPopulated = new List<GameObject>();
+
+    private readonly static PopulatePlants instance;
+
+    // Private constructor
+    private PopulatePlants() { }
+
+    // static constructor
+    static PopulatePlants()
+    {
+        PopulatePlants.instance = new PopulatePlants();
+    }
+
+    public static PopulatePlants Instance
+    {
+        get
+        {
+            return PopulatePlants.instance;
+        }
+    }
+
+    // Method to add plants to the list
+    public void AddPlant(GameObject aPlant)
+    {
+        PlantsPopulated.Add(aPlant);
+    }
+
+    public GameObject getPlants(int i)
+    {
+        return PlantsPopulated[i];
+    }
+
+
+
+    public void generateRandomPosition()
+    {
+
+
+    }
+
+
+}
+
