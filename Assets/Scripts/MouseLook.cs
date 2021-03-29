5113dc5c44d60851cf7f34aec47e3a4f9f4a4ed2
Ellie McDonald <elliejeanmcd@gmail.com>
2021-03-29 11:43:05 -0500

    Fixed camera motion to keep player in view

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -27,15 +27,9 @@ public class MouseLook : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY; // if += rotation is opposite of desired direction
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // prevents over rotations so the camera doesn't rotate behind the player
+        xRotation = Mathf.Clamp(xRotation, -45f, 45f); // prevents over rotations so the camera doesn't rotate behind the player
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
 
         playerBody.Rotate(Vector3.up * mouseX);
-    }
-
-    // Makes sure the camera moves last - meaning, after the player moves
-    void LateUpdate()
-    {
-        transform.position = playerBody.transform.position + offset;
     }
 }
